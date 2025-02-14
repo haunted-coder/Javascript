5673c02de73486809b0d665f213f917dbe239f96
haunted-coder <132993617+haunted-coder@users.noreply.github.com>
2025-02-14 13:14:48 +0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+var flowers = ["Lily", "Water lily", "Rose" ,"Tulip" ,"China rose", "Paper flower", "Belly"  ];
+
+document.getElementById("myj").innerHTML = flowers.join("  and  ");
+document.getElementById("myf").innerHTML = flowers;
+
+
+
+
