29ec2665c761f5157dbb7ea67ef0180a835909e7
haunted-coder <132993617+haunted-coder@users.noreply.github.com>
2025-02-27 23:30:11 +0600

    Add files via upload

--- script.js
+++ script.js
@@ -1,12 +1,11 @@
-var str="Hello how are you doing for the day";
+var text= ""
+var i=0;
 
-var search_position=str.search("doing");
+random(i<10) 
+{
+text=text + "The values inside the block" +i+<br></br>
+i++;
 
-
-
-
-
-
-
-
-
+}
+document.getElementById("res").innerHTML=text;
+document.write("The value of i is outside the blockis:"+1).innerHTML=text;
