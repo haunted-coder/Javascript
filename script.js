c44e11b3090a1d963f64db3c117d11fb0ecffcd0
haunted-coder <132993617+haunted-coder@users.noreply.github.com>
2025-03-07 15:54:18 +0600

    Add files via upload

--- script.js
+++ script.js
@@ -1,12 +1,15 @@
-function validateform() {
+var choice = prompt("Welcome to Area Calculator.  \n please enter your choice \n 1. AREA OF RECTANGLE.\n 2. AREA OF CIRCLE. \n 3. AREA OF PARALLELLOGRAM \n 4. AREA OF TRIANGLE")
+
+if(choice == '1') {
 
 
-    var x = document.forms["myform"]["fname"].value;
-    if (x == "") {
-        alert("Name must be filled out");
-        return false;
-      } else document.write("Name submitted");
-
+var length =prompt('ENTER THE LENGTH');
+var BREADTH =prompt('ENTER THE BREADTH');
+var RESULT = Number(length)* Number(BREADTH)
+alert("The area is " + RESULT)
 
 
 }
+ 
+
+
