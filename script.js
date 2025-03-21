31900d0217c3b5b6a5df0b1afff881aa9de74034
haunted-coder <132993617+haunted-coder@users.noreply.github.com>
2025-03-21 15:53:58 +0600

    Add files via upload

--- script.js
+++ script.js
@@ -1,16 +1,42 @@
-function getRandomColor() {
-    const letters = '0123456789ABCDEF';
-    let color = '#';
-    for (let i = 0; i < 6; i++) {
-        color += letters[Math.floor(Math.random() * 16)];
-    }
-    return color;
+window.onload  = function(){
+    var seconds = 0;
+    var milliseconds = 0;
+
+
+    var appendmilliseconds = document.getElementById("millisecond").innerHTML
+    var appendseconds = document.getElementById("second").innerHTML
+    var buttonstop = document.getElementById("button-stop").innerHTML
+    var buttonstart = document.getElementById("button-start").innerHTML
+    var buttonreset = document.getElementById("button-reset").innerHTML
+    var Interval;
+
+
+
+buttonstart.onclick=function(){
+
+clearInterval(interval);
+interval = setInterval(startTimer, 10);
+};
+
+
+buttonstop.onclick=function(){
+
+    clearInterval(interval);
+};
+buttonreset.onclick=function(){
+
+    clearInterval(interval);
+   milliseconds = 0;
+   seconds =0;
+   appendmilliseconds.innerHTML ="00"
+   appendseconds.innerHTML = "00"
+}
+function startTimer(){
+milliseconds++;
+
+
+
+
 }
 
-
-function changeBackgroundColor() {
-    document.body.style.backgroundColor = getRandomColor();
-}
-
-
-setInterval(changeBackgroundColor, 1000);
+};
