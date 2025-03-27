2308d5e0a7de14453811385c36557ca436c46a76
haunted-coder <132993617+haunted-coder@users.noreply.github.com>
2025-03-27 16:32:07 +0600

    Add files via upload

--- script.js
+++ script.js
@@ -1,57 +1,22 @@
-window.onload  = function(){
-    var seconds = 0;
-    var milliseconds = 0;
+var myform= document.getElementById("myfom")
+var myinput=document.getElementById("myinput")
+var myitem=document.getElementById("myitem")
 
+myform.addEventListener("submit"
 
-    var appendmilliseconds = document.getElementById("millisecond").innerHTML
-    var appendseconds = document.getElementById("second").innerHTML
-    var buttonstop = document.getElementById("button-stop").innerHTML
-    var buttonstart = document.getElementById("button-start").innerHTML
-    var buttonreset = document.getElementById("button-reset").innerHTML
-    var Interval;
-
-
-
-buttonstart.onclick=function(){
-
-clearInterval(interval);
-interval = setInterval(startTimer, 10);
-};
-
-
-buttonstop.onclick=function(){
-
-    clearInterval(interval);
-};
-buttonreset.onclick=function(){
-
-    clearInterval(interval);
-   milliseconds = 0;
-   seconds =0;
-   appendmilliseconds.innerHTML ="00"
-   appendseconds.innerHTML = "00"
-}
-function startTimer(){
-milliseconds++;
-if (milliseconds <=9) {
- {
-
-        } else {
-            isRunning = false;
-            startStopButton.textContent = 'Start';
-            runningTime += difference;
-            clearInterval(tInterval);
-        }
-    }
+function (event){
+event.preventDefault()
+createitem(myInput.value)
+})
 
 
 
 
-    
-}
 
 
 
-}
 
-};
+
+
+
+
